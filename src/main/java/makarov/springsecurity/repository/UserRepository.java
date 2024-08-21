634485ec8d0701af701355795b9a504431724811
Makarov93 <153182603+Makarov93@users.noreply.github.com>
2024-08-22 00:56:10 +0500

    2nd try

--- src/main/java/makarov/springsecurity/repository/UserRepository.java
+++ src/main/java/makarov/springsecurity/repository/UserRepository.java
@@ -2,8 +2,10 @@ package makarov.springsecurity.repository;
 
 import makarov.springsecurity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("Select u from User u left join fetch u.roles where u.username=:username")
     User findByUsername(String username);
 }
 
